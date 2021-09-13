a5f9eb5a7cb81445ed3ffc5d7692596f926416bb
Jacob Nyyssønen Henriksen <jaco9515@zbc.dk>
2021-09-13 14:35:27 +0200

    New Engine. .Net Core 5.0

---
+++ Ironfall_Engine2/Models/LocalPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ironfall_Engine.Models
+{
+    public class LocalPlayer : LivingEntity
+    {
+        public string CharacterClass { get; set; }
+        public string UserID { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int StatBody { get; set; }
+        public int StatSpirit { get; set; }
+        public int StatFellowship { get; set; }
+
+
+    }
+}
---
+++ Ironfall_Engine2/Models/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ironfall_Engine.Models
+{
+    public class Location
+    {
+
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageName { get; set; }
+        public bool IsTravelAllowed { get; set; }
+
+        public Location(int xCoordinate, int yCoordinate, string name, string description, string imageName, bool isTravelAllowed)
+        {
+            XCoordinate = xCoordinate;
+            YCoordinate = yCoordinate;
+            Name = name;
+            Description = description;
+            ImageName = imageName;
+            IsTravelAllowed = isTravelAllowed;
+        }
+
+    }
+}
