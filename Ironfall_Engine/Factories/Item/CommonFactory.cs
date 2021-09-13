c112fccb9671d5a30123a8cef23105afd63045f3
Joachim <joac3146@zbc.dk>
2021-09-13 12:45:11 +0200

    Added ItemFactories

---
+++ Ironfall_Engine/Factories/Item/CommonFactory.cs
@@ -0,0 +1,21 @@
+﻿using Ironfall_Engine.Models.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ironfall_Engine.Factories.Item
+{
+    class CommonFactory
+    {
+        static int id = 400;
+
+        public Common Create(string name, string description, int value, bool isUnique, Enum type, int quantity)
+        {
+            id += 1;
+
+            return new Common(id, name, description, value, isUnique, type, quantity);
+        }
+    }
+}
