cb4d56895ca1ae2d9e3ac4bb921153775396da74
Joachim <joac3146@zbc.dk>
2021-09-13 11:47:18 +0200

    Revert "Added more locations and the random number generator."
    This reverts commit 7589d4863b148e5c852b8ea4b8eda745b1d3da55.

--- Ironfall_Engine/Factories/WorldFactory.cs
+++ Ironfall_Engine/Factories/WorldFactory.cs
@@ -15,14 +15,6 @@ namespace Ironfall_Engine.Factories
 
             newWorld.AddLocation(0, 0, "Home", "Test Location", "home.jpg");
 
-            newWorld.AddLocation(0, 1, "Town square",
-                "You see a fountain here and two shops one to the north and one to the east. You live to the south",
-                "TownSquare.jpg");
-
-            newWorld.AddLocation(-1, 1, "Trading Shop",
-                "The shop of a trader.",
-                "Shop.jpg");
-
             return newWorld;
         }
     }
