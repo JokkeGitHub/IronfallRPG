a301248af2943d5af7c849c9e2c92683e2f41e79
Joachim <joac3146@zbc.dk>
2021-09-13 10:48:30 +0200

    Merged

---
+++ Ironfall_Engine/Factories/WorldFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ironfall_Engine.Models;
+
+namespace Ironfall_Engine.Factories
+{
+    internal static class WorldFactory
+    {
+        internal static World CreateWorld()
+        {
+            World newWorld = new World();
+
+            newWorld.AddLocation(0, 0, "Home", "Test Location", "home.jpg");
+
+            newWorld.AddLocation(0, 1, "Town square",
+                "You see a fountain here and two shops one to the north and one to the east. You live to the south",
+                "TownSquare.jpg");
+
+            newWorld.AddLocation(-1, 1, "Trading Shop",
+                "The shop of a trader.",
+                "Shop.jpg");
+
+            return newWorld;
+        }
+    }
+}
---
+++ Ironfall_Engine/RNG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ironfall_Engine
+{
+    class RNG
+    {
+        // The Random Number Generator
+
+        private static readonly Random _simpleNumberGenerator = new Random();
+
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _simpleNumberGenerator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
