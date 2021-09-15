8e6124a3c09039bc5e95d864200b698bcd403586
Jacob Nyyssønen Henriksen <Henriksen.Jacob@gmail.com>
2021-09-15 10:27:51 +0200

    Begining work to test basic combat and game messages.

--- Ironfall_Engine/ViewModels/GameSession.cs
+++ Ironfall_Engine/ViewModels/GameSession.cs
@@ -33,17 +33,23 @@ namespace Ironfall_Engine.ViewModels
 
         public GameSession()
         {
-            CurrentPlayer = new LocalPlayer();
-            CurrentPlayer.Name = "Happy New Adventurer";
-            CurrentPlayer.CharacterClass = "No Class";
-            CurrentPlayer.HpMax = 10;
-            CurrentPlayer.HpCurrent = CurrentPlayer.HpMax;
-            CurrentPlayer.Gold = 1000000;
-            CurrentPlayer.ExperiencePoints = 0;
-            CurrentPlayer.Level = 1;
-            CurrentPlayer.StatBody = 1;
-            CurrentPlayer.StatSpirit = 1;
-            CurrentPlayer.StatFellowship = 1;
+            CurrentPlayer = new LocalPlayer(
+                "Classless",
+                "UserID", 
+                0,
+                1,
+                1,
+                1,
+                "Happy New Adventurer",
+                "player picture",
+                10, 10,
+                1,2,
+                5,5,
+                5,5,
+                1,
+                1,
+                0);
+           
 
             CurrentWorld = WorldFactory.CreateWorld();
             CurrentLocation = CurrentWorld.LocationAt(0, 0);
