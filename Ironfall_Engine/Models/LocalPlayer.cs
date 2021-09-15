747011b88eae8dfef24c012430272baa4add69f5
Jacob Nyyssønen Henriksen <Henriksen.Jacob@gmail.com>
2021-09-15 08:58:49 +0200

    Mine typo fixes

--- Ironfall_Engine/Models/LocalPlayer.cs
+++ Ironfall_Engine/Models/LocalPlayer.cs
@@ -8,6 +8,7 @@ namespace Ironfall_Engine.Models
 {
     public class LocalPlayer : LivingEntity
     {
+
         public string CharacterClass { get; set; }
         public string UserID { get; set; }
         public int ExperiencePoints { get; set; }
@@ -15,6 +16,16 @@ namespace Ironfall_Engine.Models
         public int StatSpirit { get; set; }
         public int StatFellowship { get; set; }
 
+        public LocalPlayer(string characterClass, string userID, int experiencePoints, int statBody, int statSpirit, int statFellowship, string name, string image, int hpMax, int hpCurrent, int damageMinimum, int damageMaximum, int mpMax, int mpCurrent, int apMax, int apCurrent, int defence, int level, int gold) : 
+            base(name, image, hpMax, hpCurrent, damageMinimum, damageMaximum, mpMax, mpCurrent, apMax, apCurrent, defence, level, gold)
+        {
+            CharacterClass = characterClass;
+            UserID = userID;
+            ExperiencePoints = experiencePoints;
+            StatBody = statBody;
+            StatSpirit = statSpirit;
+            StatFellowship = statFellowship;
+        }
 
     }
 }
--- Ironfall_Engine/Models/Monster.cs
+++ Ironfall_Engine/Models/Monster.cs
@@ -15,7 +15,7 @@ namespace Ironfall_Engine.Models
         public int RewardExp { get; set; }
 
         public Monster(string name, string image, int hpCurrent, int hpMax, int damageMin, int damageMax, int mpCurrent, int mpMax, int apCurrent, int apMax, int defence, int level, int gold, string type, string subType, string description, int rewardExp) 
-            : base(name, image, hpCurrent, hpMax, damageMin, damageMax, mpCurrent, mpMax, apCurrent, apMax, defence, level, gold)
+            : base(name, image, hpMax, hpCurrent, damageMin, damageMax, mpMax, mpCurrent, apMax, apCurrent, defence, level, gold)
         {
             Image = $"/Ironfall_Engine;component/Resource/Images/Monsters/{image}";
             Type = type;
