ad02499c85874cb623488da328e637c3fca0ebd3
Jacob Nyyssønen Henriksen <Henriksen.Jacob@gmail.com>
2021-10-08 08:11:44 +0200

    Minor changes so the program works again

---
+++ Ironfall_Engine/Factories/DialogFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ironfall_Engine.Models;
+
+namespace Ironfall_Engine.Factories
+{
+    class DialogFactory
+    {
+        /*
+        public Dialog CreateNpcDialog(string dialogNpc, int dialogId, string dialogText, int responseNmb)
+        {
+
+        }
+        */
+    }
+}
---
+++ Ironfall_Engine/Models/Dialog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ironfall_Engine.Models
+{
+    class Dialog
+    {
+        public Npc DialogNpc { get; set; }
+        public int DialogId { get; set; }
+        public string DialogText { get; set; }
+        public bool IsRecurring { get; set; }
+        public int ResponseNmb { get; set; }
+
+
+        public Dialog(Npc dialogNpc, int dialogId, string dialogText, bool isRecurring, int responseNmb)
+        {
+            DialogNpc = dialogNpc;
+            DialogId = dialogId;
+            DialogText = dialogText;
+            IsRecurring = isRecurring;
+            ResponseNmb = responseNmb;
+        }
+    }
+}
